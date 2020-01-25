d1eeab3d8f97f98a73e800090521e39e6cfc08a3
Przemek Baranowski <przemekbaranowski@gmail.com>
2020-01-25 12:26:48 +0100

    Ex.1 - done

--- src/main/java/pl/sda/javakrk25/practising/ex_1/Ex_1_printing_shapes.java
+++ src/main/java/pl/sda/javakrk25/practising/ex_1/Ex_1_printing_shapes.java
@@ -9,6 +9,38 @@ public class Ex_1_printing_shapes {
      * @param args
      */
     public static void main(String[] args) {
+        square(10);
+        triangle(10);
+        triangle2(11);
+    }
 
+
+    private static void triangle2(int size) {
+        //todo
+    }
+
+    private static void triangle(int size) {
+        for (int i = 0; i < size; i++) { // przejście po wierszach
+            for (int j = 0; j < size; j++) { // po kolumnach
+                if (j > i) {
+                    System.out.print(" # ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    private static void square(int size) {
+        for (int i = 0; i < size; i++) { // przejście linia po linii
+            for (int j = 0; j < size; j++) { //przejście po kolumnach
+                System.out.print(" # ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
