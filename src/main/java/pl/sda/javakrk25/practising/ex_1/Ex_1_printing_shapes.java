d37b2e63291c150fda8d63f5fca45480cdbd64a3
Przemek Baranowski <przemekbaranowski@gmail.com>
2020-01-25 12:02:40 +0100

    Ex.1 - empty

---
+++ src/main/java/pl/sda/javakrk25/practising/ex_1/Ex_1_printing_shapes.java
@@ -0,0 +1,14 @@
+package pl.sda.javakrk25.practising.ex_1;
+
+
+public class Ex_1_printing_shapes {
+
+    /**
+     * Używając symbolu #, wypisz w konsoli kształty: kwadrat, prójką prostokątny, trójkąt równoramienny
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
