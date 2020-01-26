112d001a2c93351ca4501c6b960a5948185940fd
Przemek Baranowski <przemekbaranowski@gmail.com>
2020-01-26 14:52:11 +0100

    Ex 19

---
+++ src/main/java/pl/sda/javakrk25/practising/ex_19/Ex_19.java
@@ -0,0 +1,35 @@
+package pl.sda.javakrk25.practising.ex_19;
+
+import pl.sda.javakrk25.practising.ScannerUtil;
+
+public class Ex_19 {
+
+    /**
+     * program który pobiera od użytkownika 2 liczby (A,B),
+     * a następnie liczy sumę liczb pomiędzy A,A+1,...,B
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int A = ScannerUtil.getNaturalNumber();
+        int B = ScannerUtil.getNaturalNumber();
+        System.out.println(sumInRange(A, B));
+    }
+
+    public static int sumInRange(int A, int B) {
+        if (A > B) {
+            return sumInValideRange(B, A);
+        } else {
+            return sumInValideRange(A, B);
+        }
+    }
+
+    public static int sumInValideRange(int A, int B) {
+        int sum = 0;
+        for (int i = A; i <= B; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+}
